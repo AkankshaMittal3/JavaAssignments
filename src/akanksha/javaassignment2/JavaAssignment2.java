8e4bee61eeb4e045dedf7e6df67efe2e95f4ff09
Akanksha Mittal <akanksha.mittal@zemosolabs.com>
2019-06-06 14:42:25 +0530

    Add main method and initialise checkPanagram method

---
+++ src/akanksha/javaassignment2/JavaAssignment2.java
@@ -0,0 +1,29 @@
+package akanksha.javaassignment2;
+/**
+ * Write a java function that checks if the input string contains all the letters of the alphabet a-z
+ * (case-insensitive). Write time and space complexity of your solution as comments in the source file.
+ *
+ * @author Akanksha Mittal
+ */
+
+import java.util.Scanner;
+
+public class JavaAssignment2 {
+    /**
+     * This method checks whether the input string contains all letters or not
+     *
+     * @param input    this is the string entered by user
+     */
+
+    public static void checkPanagram(String input)
+    {
+       
+    }
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter ");
+        String input = scan.nextLine();
+        checkPanagram(input);
+    }
+}
