6fa883b6ffbb157f54970ea6d278b4769c2e1581
Akanksha Mittal <akanksha.mittal@zemosolabs.com>
2019-06-07 15:20:34 +0530

    Add 5th part of the assignment

--- src/akanksha/javaincrementalassignment3/JavaIncrementalAssignment3part5.java
+++ src/akanksha/javaincrementalassignment3/JavaIncrementalAssignment3part5.java
@@ -1,4 +1,11 @@
 package akanksha.javaincrementalassignment3;
+/**
+ * Create a class with an inner class that has a non-default constructor (one that takes
+ * arguments). Create a second class with an inner class that inherits from the first inner class.
+ *
+ * @author Akanksha Mittal
+ */
+
 
 //outer class
 class A {
