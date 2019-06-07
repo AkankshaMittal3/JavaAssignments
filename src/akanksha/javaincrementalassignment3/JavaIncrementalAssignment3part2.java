0ab47deed2199061e2416c24e1bac76c60aebdcc
Akanksha Mittal <akanksha.mittal@zemosolabs.com>
2019-06-07 12:19:55 +0530

    do downcasting

--- src/akanksha/javaincrementalassignment3/JavaIncrementalAssignment3part2.java
+++ src/akanksha/javaincrementalassignment3/JavaIncrementalAssignment3part2.java
@@ -1,37 +1,41 @@
 package akanksha.javaincrementalassignment3;
 
-class Cycle
-{
-    public void balance()
-    {
+/**
+ * Create a Cycle class, with subclasses Unicycle, Bicycle and Tricycle. Add a balance( ) method
+ * to Unicycle and Bicycle, but not to Tricycle. Create instances of all three types and upcast them
+ * to an array of Cycle. Try to call balance( ) on each element of the array and observe the results.
+ * Downcast and call balance( ) and observe what happens.
+ *
+ * @author Akanksha Mittal
+ */
+
+class Cycle {
+    public void balance() {
         System.out.println("Cycle balance");
     }
 }
-class Unicycle extends Cycle
-{
-    public void balance()
-    {
+
+class Unicycle extends Cycle {
+    public void balance() {
         System.out.println("Unicycle balance");
     }
 }
-class Bicycle extends Cycle
-{
-    public void balance()
-    {
+
+class Bicycle extends Cycle {
+    public void balance() {
         System.out.println("Bicycle balance");
     }
 
 }
-class Tricycle extends Cycle
-{
+
+class Tricycle extends Cycle {
 
 }
 
 
 public class JavaIncrementalAssignment3part2 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Cycle[] cycle = new Cycle[3];
 
         Unicycle unicycle = new Unicycle();
@@ -51,6 +55,13 @@ public class JavaIncrementalAssignment3part2 {
 
         cycle[2].balance();//super class method called
 
-       
+        //downcasting
+        unicycle = (Unicycle) cycle[0];
+        bicycle = (Bicycle) cycle[1];
+        tricycle = (Tricycle) cycle[2];
+
+        unicycle.balance();
+        bicycle.balance();
+        tricycle.balance();
     }
 }
